d100998760a6c58394b32ea8eefea71287c62ca5
JiveDig <jivetheuniversal@gmail.com>
2023-10-02 16:46:21 -0400

    finalize base analytics

--- assets/js/mai-publisher-analytics.js
+++ assets/js/mai-publisher-analytics.js
@@ -4,11 +4,25 @@
  * @since 0.1.0
  */
 (function() {
+	var _paq    = window._paq = window._paq || [];
+	var siteUrl = 'https://bizbudding.info/';
+	var siteID  = '1';
+
+	// Only run if Matomo is not already loaded. Mostly for Mai Analytics.
+	if ( ! _paq.length ) {
+		(function() {
+			_paq.push( ['setTrackerUrl', siteUrl + 'matomo.php'] );
+			_paq.push( ['setSiteId', siteID] );
+
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			g.type='text/javascript'; g.async=true; g.src=siteUrl+'matomo.js'; s.parentNode.insertBefore(g,s);
+		})();
+	}
+
+
 	window.matomoAsyncInit = function() {
 		try {
-			const matomoTracker = Matomo.getTracker( 'https://analytics.bizbudding.com/matomo.php', 23 );
-
-			console.log( maiPubAnalyticsVars.dimensions );
+			const matomoTracker = Matomo.getTracker( siteUrl, siteID );
 
 			// Adds all custom dimensions passed through PHP. Must be before trackPageView.
 			for ( const key in maiPubAnalyticsVars.dimensions ) {
