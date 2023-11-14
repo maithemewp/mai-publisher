66e4554db5adf7467f1ef6a47feb8de5fa11a597
JiveDig <jivetheuniversal@gmail.com>
2023-11-14 11:44:27 -0500

    Remove forced encoding for DOMDocument

--- classes/class-output.php
+++ classes/class-output.php
@@ -680,10 +680,6 @@ class Mai_Publisher_Output {
 		// Modify state.
 		$libxml_previous_state = libxml_use_internal_errors( true );
 
-		// Encode.
-		// $html = mb_convert_encoding( $html, 'HTML-ENTITIES', 'UTF-8' );
-		$html = htmlspecialchars_decode( mb_encode_numericentity( htmlentities( $html, ENT_QUOTES, 'UTF-8' ), [0x80, 0x10FFFF, 0, ~0], 'UTF-8' ) );
-
 		// Load the content in the document HTML.
 		$dom->loadHTML( $html );
 
--- includes/functions.php
+++ includes/functions.php
@@ -68,12 +68,8 @@ function maipub_get_dom_document( $html ) {
 	// Modify state.
 	$libxml_previous_state = libxml_use_internal_errors( true );
 
-	// Encode.
-	// $html = mb_convert_encoding( $html, 'HTML-ENTITIES', 'UTF-8' );
-	$html = htmlspecialchars_decode( mb_encode_numericentity( htmlentities( $html, ENT_QUOTES, 'UTF-8' ), [0x80, 0x10FFFF, 0, ~0], 'UTF-8' ) );
-
 	// Load the content in the document HTML.
-	$dom->loadHTML( "<div>$html</div>" );
+	$dom->loadHTML( "<div>$html</div>", LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD );
 
 	// Handle wraps.
 	$container = $dom->getElementsByTagName('div')->item(0);
