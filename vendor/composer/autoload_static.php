9e1da155edbbcac73db5b2320e958f54946cda7c
JiveDig <jivetheuniversal@gmail.com>
2023-11-27 10:45:08 -0500

    update updater

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,7 +7,7 @@ namespace Composer\Autoload;
 class ComposerStaticInit87ec9a1add67e295c46dcec822c68a0c
 {
     public static $files = array (
-        'efd9d646f43178e7ba3f07758c02ce1d' => __DIR__ . '/..' . '/yahnis-elsts/plugin-update-checker/load-v5p2.php',
+        '45a16669595eb3c0a9e2994e57fc3188' => __DIR__ . '/..' . '/yahnis-elsts/plugin-update-checker/load-v5p3.php',
     );
 
     public static $classMap = array (
