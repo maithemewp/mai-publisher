cb472018f54576723be324faa4447863578bf885
JiveDig <jivetheuniversal@gmail.com>
2023-11-29 10:45:20 -0500

    only refresh via impressionable

--- assets/js/mai-publisher-ads.js
+++ assets/js/mai-publisher-ads.js
@@ -104,13 +104,17 @@ if ( window.googletag && googletag.apiReady ) {
 		apstag.fetchBids({
 			slots: uadSlots,
 			timeout: 1000,
-		}, function( bids ) {
-			// Set apstag bids, then trigger the first request to GAM.
-			googletag.cmd.push(function() {
-				apstag.setDisplayBids();
-				// googletag.pubads().refresh();
-			});
 		});
+		// apstag.fetchBids({
+		// 	slots: uadSlots,
+		// 	timeout: 1000,
+		// }, function( bids ) {
+		// 	// Set apstag bids, then trigger the first request to GAM.
+		// 	googletag.cmd.push(function() {
+		// 		apstag.setDisplayBids();
+		// 		// googletag.pubads().refresh();
+		// 	});
+		// });
 	});
 
 	// Set currently visible ads and timeout ids objects.
