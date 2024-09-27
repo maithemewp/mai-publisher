ac482f74c3d5cbbed6a910b54758406238b88ce8
JiveDig <jivetheuniversal@gmail.com>
2024-09-27 16:27:29 -0400

    Update mai_views_updated before hitting api’s

--- classes/class-views.php
+++ classes/class-views.php
@@ -272,6 +272,16 @@ class Mai_Publisher_Views {
 			wp_die();
 		}
 
+		// Update updated time.
+		switch ( $this->type ) {
+			case 'post':
+				update_post_meta( $this->id, 'mai_views_updated', $this->current );
+			break;
+			case 'term':
+				update_term_meta( $this->id, 'mai_views_updated', $this->current );
+			break;
+		}
+
 		// Get API data.
 		switch ( $this->api ) {
 			case 'matomo':
@@ -298,7 +308,7 @@ class Mai_Publisher_Views {
 			}
 
 			// If debugging, log it to error.log.
-			if ( defined( 'WP_DEBUG' ) && WP_DEBUG && defined( 'WP_DEBUG_LOG' ) && WP_DEBUG_LOG ) {
+			// if ( defined( 'WP_DEBUG' ) && WP_DEBUG && defined( 'WP_DEBUG_LOG' ) && WP_DEBUG_LOG ) {
 				error_log( 'Mai Publisher (Views) Error: ' . $return->get_error_code() . ' - ' . $return->get_error_message() );
 
 				// Check for additional error data.
@@ -308,21 +318,11 @@ class Mai_Publisher_Views {
 				if ( $error_data ) {
 					error_log( 'Mai Publisher (Views) Data: ' . print_r( $error_data, true ) );
 				}
-			}
+			// }
 
 			// Send error.
 			wp_send_json_error( $return->get_error_message(), $return->get_error_code() );
 			wp_die();
-		}
-
-		// Update updated time.
-		switch ( $this->type ) {
-			case 'post':
-				update_post_meta( $this->id, 'mai_views_updated', $this->current );
-			break;
-			case 'term':
-				update_term_meta( $this->id, 'mai_views_updated', $this->current );
-			break;
 		}
 
 		// Send it home.
