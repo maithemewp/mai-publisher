8ae98a8f49f837d8e268f3410b0730be1f275be5
JiveDig <jivetheuniversal@gmail.com>
2025-04-25 12:16:13 -0400

    v221

--- src/js/mai-publisher-ads.js
+++ src/js/mai-publisher-ads.js
@@ -34,7 +34,7 @@ let   cmpReady         = false;
 let   matomoReady      = false;
 
 // If debugging, log.
-maiPubLog( 'v220' );
+maiPubLog( 'v221' );
 
 // If we have a server-side PPID, log it.
 if ( serverPpid ) {
