3de38a8cf97077e26ce839762a2c590f65853bde
JiveDig <jivetheuniversal@gmail.com>
2025-04-29 12:01:22 -0400

    build

--- build/mai-publisher-ads.asset.php
+++ build/mai-publisher-ads.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array(), 'version' => '2ea58c75127c4a88e2cd');
-=======
-<?php return array('dependencies' => array(), 'version' => 'de98b3d112f8ae367aab');
->>>>>>> 68fb991 (remove const bidResponses)
+<?php return array('dependencies' => array(), 'version' => 'b7112a44c89ec5a50f78');
